2ac21386c2b44a42397e79da0eb186e402b23779
Sachin B <sachinbnaik448@gmail.com>
2026-06-16 20:03:50 +0530

    chore(terraform): add infrastructure outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,3 +15,31 @@ output "environment" {
 
   value = var.environment
 }
+
+
+
+
+output "alb_dns_name" {
+  description = "Application Load Balancer DNS Name"
+  value       = aws_lb.main.dns_name
+}
+
+output "ecr_repository_url" {
+  description = "Amazon ECR Repository URL"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "ECS Service Name"
+  value       = aws_ecs_service.app.name
+}
+
+output "ecs_task_definition_family" {
+  description = "ECS Task Definition Family"
+  value       = aws_ecs_task_definition.app.family
+}
